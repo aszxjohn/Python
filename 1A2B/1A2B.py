6bda419a1d0a2c59fbd2d87f721425af19e10dd3
Brad Chang <brad.chang@style.me>
2020-08-28 15:28:02 +0800

    改善1A2B中的檢查輸入錯誤的方式，讓流程更整齊 修正第一次輸入的數字沒有檢查的問題 修正重複數字的判斷方式

--- 1A2B/1A2B.py
+++ 1A2B/1A2B.py
@@ -23,18 +23,37 @@ import random
 """
 def guess_number_check(guess_number):
 
-    while len(guess_number) != 4 or guess_number[0] == '0' or guess_number == 'Number repeat' or guess_number.isnumeric() == False:
+    # 將功能模組化可以讓程式碼更好維護，檢查重複的部分邏輯較複雜，因此獨立出一個函式
+    def check_no_repeat(number):     # 這邊也可以不用傳入參數，直接使用上層的guess_number
+        for i in range(len(number)):
+            # 對每個數字都去檢查後面的數字是否有相同的字元存在
+            if number[i] in number[i+1:]:       # NOTE: 陣列中的冒號使用方式可以注意一下 (Python才有這類用法)
+                # 一旦發現有相同，就可以直接傳回錯誤，不需要將迴圈跑完
+                return False
+        # 沒有檢查到相同時，程式碼才有可能進入這裡
+        return True
+
+
+    # 原while迴圈的檢查項目缺少了相同數字的檢查，另外 guess_number == 'Number repeat' 這段顯然是錯誤的用法
+    # # while len(guess_number) != 4 or guess_number[0] == '0' or guess_number == 'Number repeat' or guess_number.isnumeric() == False:
+    while len(guess_number) != 4 or guess_number[0] == '0' or guess_number == 'Number repeat' or guess_number.isnumeric() == False or check_no_repeat(guess_number) == False:
         if guess_number[0] == '0':
             guess_number = str(input("請重新輸入~~開頭不能為0!! \n" + "1.第一個數字不能為零! \n" + "2.請不要輸入重複的數字 \n" + "3.請輸入四位數(0-9)："))
         elif guess_number.isnumeric() == False:
             guess_number = str(input("請重新輸入~~請使用數字輸入!! \n" + "1.第一個數字不能為零! \n" + "2.請不要輸入重複的數字 \n" + "3.請輸入四位數(0-9)："))
-        else:
+        elif len(guess_number) != 4:  # 延續while的檢查項目
             guess_number = str(input("請重新輸入~~請輸入四位數喔!! \n" + "1.第一個數字不能為零! \n" + "2.請不要輸入重複的數字 \n" + "3.請輸入四位數(0-9)："))
+        elif check_no_repeat(guess_number) == False: # 延續while的檢查項目
+            guess_number = str(input("請重新輸入~~請不要輸入重複的數字喔!! \n" + "1.第一個數字不能為零! \n" + "2.請不要輸入重複的數字 \n" + "3.請輸入四位數(0-9)："))
+        else:
+            # 有時候，可以加一些這類程式碼來確保程式運行是正確的，顯然邏輯正確的情況下是不可能進入這裡的
+            raise Exception("這不會發生")
         
-        for i in range(len(guess_number) - 1):
-            if guess_number[i] == guess_number[i+1]:
-                guess_number = str(input("請重新輸入~~請不要輸入重複的數字喔!! \n" + "1.第一個數字不能為零! \n" + "2.請不要輸入重複的數字 \n" + "3.請輸入四位數(0-9)："))
-                guess_number = 'Number repeat'
+        # 這段只會檢查到連續兩個數字相同，而且檢查到錯誤的時候，也不會要求使用者重新輸入
+        # # for i in range(len(guess_number) - 1):
+        # #     if guess_number[i] == guess_number[i+1]:
+        # #         guess_number = str(input("請重新輸入~~請不要輸入重複的數字喔!! \n" + "1.第一個數字不能為零! \n" + "2.請不要輸入重複的數字 \n" + "3.請輸入四位數(0-9)："))
+        # #         guess_number = 'Number repeat'
 
     return guess_number
        
@@ -65,7 +84,8 @@ def generator():
             random_number = str(random.randint(0, 9))
 
             while(random_number in result_number):
-                random_number = str(random.randint(1, 9))
+                # # random_number = str(random.randint(1, 9))  # 這裡應該是0~9?
+                random_number = str(random.randint(0, 9))
         
             result_number = str(result_number) + str(random_number)
 
@@ -76,7 +96,9 @@ def main():
     print('The anwser ：　'+ str(anwser))
 
     guess_number = guess_number_check(str(input('請輸入四位不已0開頭，也不重複的數字(0-9)')))
-    judgement_victory_conditions = ''
+    
+    # judgement_victory_conditions = ''  # 第一次完全沒有給玩家機會，沒跑judgement就直接宣告他錯
+    judgement_victory_conditions = judgement(guess_number, anwser)  # 第一次輸入的數字也要檢查才對
 
     while (judgement_victory_conditions != 'win'):
         guess_number = str(input('猜錯瞜~~ 請在猜一次 <3 (0-9)'))
