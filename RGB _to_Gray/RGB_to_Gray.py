9705164fdfeeb1f90e8d8e387fa051b41aa89375
ASZXJOHN <q2381050@gmail.com>
2020-09-07 17:18:08 +0800

    成功轉成灰階啦

--- RGB _to_Gray/RGB_to_Gray.py
+++ RGB _to_Gray/RGB_to_Gray.py
@@ -6,22 +6,26 @@ import numpy as np
 image = Image.open('C:\Workspace\Python\RGB _to_Gray\sample.jpg', mode='r')     # image is a PIL image 
 
 array1 = np.array(image)          # array is a numpy array 
-array2 = np.array(image)
-array3 = np.array(image)
-L, W, H= array1.shape             #列出陣列維度並回傳給長, 寬, 深度
-X, Y, Z = L, W, H
+array2 = np.zeros((array1.shape))
 
+l, w, h = array1.shape             #列出陣列維度並回傳給長, 寬, 深度
+i, j, k = l, w, h
 
-for X in range(0, L):
-    for Y in range(0, W): #BGR
-        array1[X, Y] = array1[X, Y] * [0.114, 0.299, 0.587]
-        array2[X, Y] = array2[X, Y] * [0.299, 0.587, 0.114]
-        array3[X, Y] = array3[X, Y] * [0.587, 0.114, 0.299]
+a, b, c = array2.shape   
+r, g, b, gr = 0, 0, 0,0
 
-image2 = Image.fromarray(array1)    # image2 is a PIL image
-image3 = Image.fromarray(array2)
-image4 = Image.fromarray(array3)
+for i in range(0, l):
+    for j in range(0, w): #BGR
+        r = array1[i, j, 0]
+        g = array1[i, j, 1]
+        b = array1[i, j, 2]
+        gr = (r*38 + g*75 + b*15) >> 7
+        array2[i, j, 0] = gr
+        array2[i, j, 1] = gr
+        array2[i, j, 2] = gr
+
+array2 = array2.astype(np.uint8)
+image2 = Image.fromarray(array2)    # image2 is a PIL image
 
 image2.show()
-image3.show()
-image4.show()
+
